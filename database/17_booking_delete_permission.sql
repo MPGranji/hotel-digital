3eff04acd9d99328e88414003436754158a95ba4
MPGranji <138967246+MPGranji@users.noreply.github.com>
2026-09-22 16:04:10 +0700

    chore: keep docs and deployment files local

--- database/17_booking_delete_permission.sql
+++
@@ -1,8 +0,0 @@
--- Allow the web application to delete eligible bookings.
--- BookingCommandService blocks deletion when a stay or financial history exists.
-SET XACT_ABORT ON;
-GO
-
-IF DATABASE_PRINCIPAL_ID(N'hotel_app') IS NOT NULL
-    GRANT DELETE ON hotel.Booking TO hotel_app;
-GO
